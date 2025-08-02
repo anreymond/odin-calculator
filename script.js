31ccdc41bf25de665d67da555646e818d6910e3b
anreymon <anreymon@gmail.com>
2025-08-02 23:56:07 +0200

    Implement basic math operation functions
    Add functions: add, subtract, multiply, and divide.
    Each takes two numeric arguments and returns the corresponding result.
    The divide function returns "ERROR" when attempting to divide by zero.

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(nb1, nb2) {
+    return nb1 + nb2;
+}
+
+function subtract(nb1, nb2) {
+    return nb1 - nb2;
+}
+
+function multiply(nb1, nb2) {
+    return nb1 * nb2;
+}
+
+function divide(nb1, nb2) {
+    if (nb2 == 0) return "ERROR";
+    else return nb1 / nb2;
+}
