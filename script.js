833fd206dc62366b6fd7867bb201bca99f1e1d41
anreymon <anreymon@gmail.com>
2025-08-03 00:11:25 +0200

    Add operate() function to evalute the operations
    Create a function `operate(nb1, nb2, operator)` that calls the
    corresponding math function based on the given operator.
    Handles invalid operators by returning an error message.

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function divide(nb1, nb2) {
     if (nb2 == 0) return "ERROR";
     else return nb1 / nb2;
 }
+
+function operate(nb1, nb2, operator) {
+    switch (operator) {
+        case "+":
+            return add(nb1, nb2);
+        case "-":
+            return subtract(nb1, nb2);
+        case "*":
+            return multiply(nb1, nb2);
+        case "÷":
+            return divide(nb1, nb2);
+        default:
+            return "ERROR: Unknown operator";
+    }
+}
